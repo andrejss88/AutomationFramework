c51a7443fd4ce63688ce239c6684f1f5cc6224ff
andrejs <andrejss88@gmail.com>
2020-01-19 18:31:02 +0100

    improve exception handling

--- backend/src/main/java/com/github/utils/HttpHelper.java
+++ backend/src/main/java/com/github/utils/HttpHelper.java
@@ -12,22 +12,24 @@ import org.json.JSONObject;
 import org.testng.TestException;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Objects;
 
 import static com.github.Constants.BASE_API_URL_NO_PROTOCOL;
+import static java.lang.String.format;
 
 public class HttpHelper {
 
-    private HttpHelper(){
+    private HttpHelper() {
 
     }
 
     public static JSONArray getReposForUser(String user) {
 
         URI uri;
-        JSONArray repos = null;
+        JSONArray repos;
 
         try {
             uri = new URIBuilder()
@@ -43,10 +45,11 @@ public class HttpHelper {
             repos = new JSONArray(json);
 
         } catch (URISyntaxException e) {
-            e.printStackTrace();
             throw new IllegalArgumentException("Supplied user is invalid");
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            throw new UncheckedIOException("Could not parse HttpResponse", e);
+        } catch (JSONException e) {
+            throw new RuntimeException("Could not create a JsonArray with extracted Json");
         }
         return Objects.requireNonNull(repos);
     }
@@ -54,10 +57,8 @@ public class HttpHelper {
     public static boolean valueIsPresent(JSONArray array, String key, String value) throws JSONException {
         for (int i = 0; i < array.length(); i++) {
             JSONObject obj = array.getJSONObject(i);
-            if(obj.has(key)){
-                if(value.equals(obj.optString(key))){
-                    return true;
-                }
+            if (obj.has(key) && value.equals(obj.optString(key))) {
+                return true;
             }
         }
         return false;
@@ -72,7 +73,7 @@ public class HttpHelper {
         int statusCode = response.getStatusLine().getStatusCode();
         if (statusCode != 200) {
             String reason = response.getStatusLine().getReasonPhrase();
-            throw new TestException(String.format("Expected Status 200, but got: '%d' with reason: %s ", statusCode, reason));
+            throw new TestException(format("Expected Status 200, but got: '%d' with reason: %s ", statusCode, reason));
         }
         return Objects.requireNonNull(response);
     }
